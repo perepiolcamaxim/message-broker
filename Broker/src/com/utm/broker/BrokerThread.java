78ac16561fabdfeff4d2c7905a316913823ab2a7
Perepiolca Maxim <perepiolcamaxim@gmail.com>
2020-09-23 20:28:40 +0300

    Receiver is removed from ConnectionStorage when disconnects.

--- Broker/src/com/utm/broker/BrokerThread.java
+++ Broker/src/com/utm/broker/BrokerThread.java
@@ -20,10 +20,12 @@ public class BrokerThread implements Runnable
     @Override
     public void run()
     {
+        PrintWriter writer = null;
+        BufferedReader reader = null;
         try
         {
-            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            writer = new PrintWriter(clientSocket.getOutputStream());
+            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String inputLine;
             int outputLine;
 
@@ -34,25 +36,32 @@ public class BrokerThread implements Runnable
 
                 outputLine = handler.handle(clientSocket, payload);
 
-                if(outputLine == 1)
+                if (outputLine == 1)
                 {
                     writer.println("Payload received!");
                     writer.flush();
-                }
-                else
+                } else
                 {
                     writer.println("Hello receiver!");
                     writer.flush();
                 }
             }
-
-            writer.close();
-            reader.close();
-            clientSocket.close();
         }
         catch (IOException e)
         {
             System.out.println("Client disconected");
+            try
+            {
+                writer.close();
+                reader.close();
+                ConnectionStorage.remove(clientSocket);
+                clientSocket.close();
+            }
+            catch (IOException ioException)
+            {
+                ioException.printStackTrace();
+                System.out.println("Can't clone the socket!");
+            }
         }
     }
 }
